bcd82e9debbc45d7eadff047efbe3421da182a2b
mafrarrix <devapp.vs@outlook.com>
2024-06-17 02:39:23 +0200

    Fix: updated dns terraform configuration

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -1,10 +1,10 @@
-resource "digitalocean_domain" "example_domain" {
+resource "digitalocean_domain" "domain" {
   name = "mafrarrix.shop"
 }
 
 resource "digitalocean_record" "lb_dns" {
   count = 2
-  domain = digitalocean_domain.example_domain.name
+  domain = digitalocean_domain.domain.name
   type   = "A"
   name   = "web-${count.index}"
   value = digitalocean_loadbalancer.lb.ip
