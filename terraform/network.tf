b6cba32a1f13b7c313329367cbe814416c4644a9
mafrarrix <devapp.vs@outlook.com>
2024-06-17 02:46:59 +0200

    Fix: update port LB terraform

--- terraform/network.tf
+++ terraform/network.tf
@@ -5,7 +5,7 @@ resource "digitalocean_loadbalancer" "lb" {
   forwarding_rule {
     entry_port     = 80
     entry_protocol = "http"
-    target_port    = 80
+    target_port    = 8080
     target_protocol = "http"
   }
 
