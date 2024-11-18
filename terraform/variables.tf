af794a862cc328557fbb276b17fd799e4a43fc5e
mafrarrix <devapp.vs@outlook.com>
2024-11-18 23:03:48 +0100

    fix: readded variable terraform config

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,4 +1,16 @@
 
+variable "do_token" {
+  description = "Digitalocean token"
+  type        = string
+  sensitive   = true
+  
+}
+variable "pvt_key" {
+  description = "Path private key"
+  type        = string
+  sensitive   = true
+
+}
 variable "db_password" {
   description = "Password for the database"
   type        = string
