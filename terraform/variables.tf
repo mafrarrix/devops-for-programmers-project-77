3d258cf746fcb525dca190686edc270bc90c5c56
mafrarrix <devapp.vs@outlook.com>
2024-11-18 22:40:26 +0100

    fix: terraform config

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,14 +1,3 @@
-variable "do_token" {
-  description = "Digitalocean token"
-  type        = string
-  sensitive   = true
-}
-
-variable "pvt_key" {
-  description = "Path private key"
-  type        = string
-  sensitive   = true
-}
 
 variable "db_password" {
   description = "Password for the database"
