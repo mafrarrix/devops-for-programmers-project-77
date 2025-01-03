93afc9ab76f54f7ef7346e36a5553eae0a055f67
mafrarrix <devapp.vs@outlook.com>
2025-01-03 21:41:06 +0100

    feat: first implementation  lb

--- terraform/network.tf
+++ terraform/network.tf
@@ -29,3 +29,35 @@ resource "digitalocean_record" "lb_dns" {
   name   = "web-${count.index}"
   value = digitalocean_loadbalancer.lb.ip
 } */
+
+# Creazione del bilanciatore di carico Scaleway
+resource "scaleway_lb_ip" "main" {
+  zone = "fr-par-2"
+}
+
+resource "scaleway_lb" "base" {
+  ip_ids = [scaleway_lb_ip.main.id]
+  zone   = scaleway_lb_ip.main.zone
+  type   = "LB-S"
+}
+
+resource "scaleway_lb_backend" "backend01" {
+  lb_id            = scaleway_lb.base.id
+  name             = "backend01"
+  forward_protocol = "http"
+  forward_port     = "8080"
+  
+}
+
+resource "scaleway_lb_frontend" "frontend01" {
+  lb_id        = scaleway_lb.base.id
+  backend_id   = scaleway_lb_backend.backend01.id
+  name         = "frontend01"
+  inbound_port = "80"
+}
+
+resource "scaleway_lb_route" "rt01" {
+  frontend_id       = scaleway_lb_frontend.frontend01.id
+  backend_id        = scaleway_lb_backend.backend01.id
+  match_host_header = "mafrarrix.shop"
+}
