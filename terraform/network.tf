539e2afa93286d97c6c0780f6f8498fa1464b22f
mafrarrix <devapp.vs@outlook.com>
2025-01-19 21:40:33 +0100

    fix: removed old config network

--- terraform/network.tf
+++ terraform/network.tf
@@ -1,35 +1,3 @@
-/* resource "digitalocean_loadbalancer" "lb" {
-  name   = "example-lb"
-  region = "fra1"
-
-  forwarding_rule {
-    entry_port     = 8080
-    entry_protocol = "http"
-    target_port    = 80
-    target_protocol = "http"
-  }
-
-  healthcheck {
-    port     = 8080
-    protocol = "http"
-    path     = "/"
-  }
-
-  droplet_ids = digitalocean_droplet.web.*.id
-}
-
-resource "digitalocean_domain" "domain" {
-  name = "mafrarrix.shop"
-}
-
-resource "digitalocean_record" "lb_dns" {
-  count = 2
-  domain = digitalocean_domain.domain.name
-  type   = "A"
-  name   = "web-${count.index}"
-  value = digitalocean_loadbalancer.lb.ip
-} */
-
 # Creazione del bilanciatore di carico Scaleway
 resource "scaleway_lb_ip" "main" {
   zone = "fr-par-2"
@@ -41,6 +9,7 @@ resource "scaleway_lb" "base" {
   type   = "LB-S"
 }
 
+# Chi riceverà il traffico
 resource "scaleway_lb_backend" "backend01" {
   lb_id            = scaleway_lb.base.id
   name             = "backend01"
@@ -49,6 +18,7 @@ resource "scaleway_lb_backend" "backend01" {
   
 }
 
+# Punto di esposizione per l'accettazione del traffico
 resource "scaleway_lb_frontend" "frontend01" {
   lb_id        = scaleway_lb.base.id
   backend_id   = scaleway_lb_backend.backend01.id
